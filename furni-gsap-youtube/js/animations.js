ccb77013c8f5e7b0186ce965ed1a24391d0a6a0f
Vlad260192 <v.kapustianskiy@gmail.com>
2023-02-23 15:11:06 +0200

    base

--- furni-gsap-youtube/js/animations.js
+++ furni-gsap-youtube/js/animations.js
@@ -4,15 +4,15 @@ const tlLoader = gsap.timeline();
 
 // Loader
 
-// tlLoader
-// .set('.loader__item', {yPercent: -100,})
-// .set('.loader__title', {opacity: 0,})
-// .to('.loader__item', {yPercent: 0, duration: 0.5, stagger: 0.25,})
-// .to('.loader__item', {yPercent: 100, duration: 0.5, stagger: 0.25})
-// .to('.loader__title', {opacity: 1, duration: 1, scale: 1.2})
-// .set('.loader__item', {yPercent: -100})
-// .to('.loader__title', {opacity: 0, duration: 0, scale: 0.8})
-// .to('.loader', {yPercent: -100, duration: 1}, '+=0.2')
+tlLoader
+.set('.loader__item', {yPercent: -100,})
+.set('.loader__title', {opacity: 0,})
+.to('.loader__item', {yPercent: 0, duration: 0.5, stagger: 0.25,})
+.to('.loader__item', {yPercent: 100, duration: 0.5, stagger: 0.25})
+.to('.loader__title', {opacity: 1, duration: 1, scale: 1.2})
+.set('.loader__item', {yPercent: -100})
+.to('.loader__title', {opacity: 0, duration: 0, scale: 0.8})
+.to('.loader', {yPercent: -100, duration: 1}, '+=0.2')
 
 // Laptop
 
